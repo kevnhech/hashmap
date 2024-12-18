ce3552589d75c35a67e41f51348cb6dc272034c7
kevin <kevnhech@gmail.com>
2024-12-18 15:38:08 -0800

    Create hash(key) function

--- hashmap.js
+++ hashmap.js
@@ -3,4 +3,15 @@ class HashMap {
     this.loadFactor = 0;
     this.capacity = 0;
   }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
 }
